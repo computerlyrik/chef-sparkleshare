cd4266df7e19ddbf647a2a9748effdd83fbaeafd
Christian Fischer <sw-dev@computerlyrik.de>
2013-03-27 01:56:07 +0100

    added ssl dependency for secret generation

--- metadata.rb
+++ metadata.rb
@@ -6,4 +6,5 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.6.0"
 
 depends           "openssh"
+depends           "openssl"
 depends           "npm"
